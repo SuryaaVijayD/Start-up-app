b2026992480982ad7431a27a3efee2102a0807d9
Suryaa <139347672+SuryaaVijayD@users.noreply.github.com>
2024-11-08 19:15:55 +0530

    Update Service.jsx

--- src/components/Service/Service.jsx
+++ src/components/Service/Service.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from "aos"
+import 'aos/dist/aos.css'
 import './Servicecss.css'; // Import the CSS file
 
 const userFeedback = [
@@ -16,10 +18,13 @@ const MobileCard = [
 
 
 const Service = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
   return (
-    <div id='Service' className='h-92 mt-32'>
-      <h1 className='text-center text-3xl mb-20'>Services & Feedback</h1>
-      <div className='hidden md:block'>
+    <div id='Service' className='h-92 mt-24 bg-gray-100 py-8'>
+      <h1 className='text-center text-3xl mb-20' data-aos="fade-up">Services & Feedback</h1>
+      <div className='hidden md:block' data-aos="fade-left">
         <div className='scroll-container mb-11'>
           <div className='scroll-wrapper'>
             {userFeedback.map((card) => (
@@ -88,19 +93,19 @@ const Service = () => {
         </div>
       </div>
       <div>
-        {MobileCard.map((card)=>(
+        {MobileCard.map((card) => (
           <div key={card.id} className='mb-8 mx-6 p-4 rounded-md shadow-lg md:hidden'>
-          <div className='flex items-center justify-center'>
-            <div className='avatar'></div>
-            <div className='ml-4'>
-              <h1 className='text-lg'>{card.name}</h1>
-              <p className='text-gray-400'>{card.days}</p>
+            <div className='flex items-center justify-center'>
+              <div className='avatar'></div>
+              <div className='ml-4'>
+                <h1 className='text-lg'>{card.name}</h1>
+                <p className='text-gray-400'>{card.days}</p>
+              </div>
+            </div>
+            <div className='mt-2'>
+              <p>{card.description}</p>
             </div>
           </div>
-          <div className='mt-2'>
-            <p>{card.description}</p>
-          </div>
-        </div>
         ))}
       </div>
     </div>
