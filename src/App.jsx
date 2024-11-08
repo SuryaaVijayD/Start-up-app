a9851bfe3fadbb859fc9b2f5d40f7fbf9d7e19dc
Suryaa <139347672+SuryaaVijayD@users.noreply.github.com>
2024-11-08 19:08:42 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,22 @@ import './App.css'
 import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
 import NavBar from "./components/Navbar/NavBar"
-import Resolution from './components/Revolution/Resolution'
+import Regsiter from './components/RegisterSection/Regsiter'
+import About from './components/Revolution/About'
 import Service from './components/Service/Service'
 
 function App() {
 
   return (
-      <div>
-        <NavBar/>
-        <Home/>
-        <Resolution/>
-        <Service/>
-        <Contact/>
-        {/* 
-        
-        
-        < */}
-      </div>
-    )
+    <div>
+      <NavBar />
+      <Home />
+      <About />
+      <Service />
+      <Regsiter />
+      <Contact />
+    </div>
+  )
 }
 
 export default App
