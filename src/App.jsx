ba55206f87b613531e03c8638103c59f10d9c3ad
Suryaa <139347672+SuryaaVijayD@users.noreply.github.com>
2024-11-08 19:24:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 import Contact from './components/Contact/Contact'
 import Home from './components/Home/Home'
 import NavBar from "./components/Navbar/NavBar"
 import Regsiter from './components/RegisterSection/Regsiter'
 import About from './components/Revolution/About'
+import Loader from './components/Loader/Loader'
 import Service from './components/Service/Service'
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
   return (
     <div>
       <NavBar />
