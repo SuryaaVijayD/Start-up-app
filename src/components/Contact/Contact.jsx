9d828398b6972a9a1d2e608e31b677ba02216431
Suryaa <139347672+SuryaaVijayD@users.noreply.github.com>
2024-11-08 19:10:53 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,36 +1,42 @@
-import React from 'react'
-import "./Contactcss.css"
-import { MdOutlineArrowOutward } from "react-icons/md";
+import React, { useEffect } from 'react';
+import AOS from "aos"
+import 'aos/dist/aos.css'
+import "./Contactcss.css";
 
 import { FaInstagram } from "react-icons/fa6";
 import { FaFacebookF } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
 
-
 const Contact = () => {
-  return (
-    <div id='Contact' className='relative'>
-        <div className='h-72 px-6 text-center mt-60 mb-20 flex justify-center items-center flex-col'>
-            <h1 className='text-3xl font-semibold md:text-5xl mb-5'>To Have Conversation to collabrate</h1>
-            <p className='text-md md:text-xl mb-5'>Lorem ipsum dolor sit amet, concenter</p>
-            <button type='button' className='flex mt-2 px-7 text-xl py-3 text-white bg-black rounded-full'>Reach out <MdOutlineArrowOutward className='ml-2 self-center'/></button>        
-        </div>
-        <div className='flex md:justify-between md:flex-row flex-col mb-9 md:px-32 px-6'>
-            <div>
-                <h1 className='text-md md:text-xl font-medium text-center mb-4'>@copyright 2024</h1>
+
+    useEffect(() => {
+        AOS.init({ duration: 1500 });
+    }, []);
+
+    return (
+        <div id='Contact' className='relative'>
+            <div className='h-72 px-6 text-center mt-60 mb-20 flex justify-center items-center flex-col' data-aos="fade-up">
+                <h1 className='text-3xl font-semibold md:text-5xl mb-5'>To Have Conversation to Collaborate</h1>
+                <p className='text-md md:text-xl mb-5'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                <button type='button' className='flex mt-2 px-7 text-xl py-3 text-white bg-black rounded-full'>Reach out</button>
             </div>
-            <div className='flex mb-4 justify-center'>
-                <FaFacebookF className="text-2xl md:text-3xl mr-4"/>
-                <FaInstagram className="text-2xl md:text-3xl mr-4"/>
-                <FaXTwitter className="text-2xl md:text-3xl mr-4"/>
-                <FaLinkedin className="text-2xl md:text-3xl mr-4"/>
+            <div className='flex md:justify-between md:flex-row flex-col mb-9 md:px-32 px-6'>
+                <div>
+                    <h1 className='text-md md:text-xl font-medium text-center mb-4'>© 2024</h1>
+                </div>
+                <div className='flex mb-4 justify-center'>
+                    <FaFacebookF className="text-2xl md:text-3xl mr-4" />
+                    <FaInstagram className="text-2xl md:text-3xl mr-4" />
+                    <FaXTwitter className="text-2xl md:text-3xl mr-4" />
+                    <FaLinkedin className="text-2xl md:text-3xl mr-4" />
+                </div>
             </div>
+            {/* Dancing letters */}
+            <div className='dancing-style text-shadow absolute large-web-text-contact1'>u</div>
+            <div className='hidden lg:block dancing-style dancing-style text-shadow absolute large-web-text-contact2'>K</div>
         </div>
-        <div className='hidden md:block dancing-style text-shadow absolute large-web-text-contact1'>u</div>
-        <div className='hidden md:block dancing-style text-shadow absolute large-web-text-contact2'>K</div>
-    </div>
-  )
+    );
 }
 
-export default Contact
+export default Contact;
